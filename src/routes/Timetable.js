4f453be6277ec36b5b5744603f4b9635dee6e4ea
prettydude <Coolboy.mc1@gmail.com>
2020-09-23 08:13:28 +0300

    More fixes

--- src/routes/Timetable.js
+++ src/routes/Timetable.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import classNames from "classNames"
+import classNames from "classnames"
 
 import { getTimetable } from "../utils/parser"
 import {setSearchParameters} from "../utils/history"
