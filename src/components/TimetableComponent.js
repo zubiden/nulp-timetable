4171a829d445a5736ac05ab6db0fade9cfa11645
zubiden <zubiden.ua@gmail.com>
2022-02-15 02:48:58 -0500

    Migrate Timetable to functional component

--- src/components/TimetableComponent.js
+++ src/components/TimetableComponent.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 import classNames from "classnames";
 import { getCurrentUADate } from '../utils/date';
@@ -12,47 +12,124 @@ import "./TimetableComponent.scss";
         content
 */
 
-class TimetableComponent extends React.Component {
-    ref = React.createRef();
+function timeToDate(time) {
+    let date = getCurrentUADate();
+    let [hours, minutes] = time.split(':');
+    date.setHours(hours);
+    date.setMinutes(minutes)
+    date.setSeconds(0);
+    return date;
+}
 
-    render() {
-        const {elements} = this.props;
-        const sizes = this.findSize(elements)
-        return (
-            <div className={classNames({
-            timetable: true,
-            "has-saturday": sizes.days === 6,
-            "has-sunday": sizes.days === 7, // Не заздрю...
-              })} ref={this.ref}>
-                {this.makeTable(sizes, elements)}
-            </div>
-        )
+function getWeekday(number) {
+    switch (number) {
+        case 1:
+            return "Понеділок";
+        case 2:
+            return "Вівторок";
+        case 3:
+            return "Середа";
+        case 4:
+            return "Четвер";
+        case 5:
+            return "П'ятниця";
+        case 6:
+            return "Субота";
+        case 7:
+            return "Неділя";
+        default:
+            return "Не вдалося отримати день"
+    }
+}
+
+function getHours(num) {
+    switch (num) {
+        case 1:
+            return ["8:30", "10:05"];
+        case 2:
+            return ["10:20", "11:55"];
+        case 3:
+            return ["12:10", "13:45"];
+        case 4:
+            return ["14:15", "15:50"];
+        case 5:
+            return ["16:00", "17:35"];
+        case 6:
+            return ["17:40", "19:15"];
+        case 7:
+            return ["19:20", "20:55"];
+        case 8:
+            return ["21:00", "22:35"];
+        default:
+            return ["", ""];
+    }
+}
+
+function findSize(elements) {
+    let days = 5;
+    let rows = 5;
+    for (let element of elements) {
+        if (element.day > days) days = element.day;
+        if (element.position > rows) rows = element.position;
+    }
+    return {
+        days: Math.min(days, 7),
+        rows
+    }
+}
+
+const TimetableComponent = ({ elements, onReady }) => {
+    const ref = useRef();
+    const sizes = findSize(elements)
+
+    const getActiveLesson = () => {
+        let date = getCurrentUADate();
+        let currentDay = date.getDay(); // 0 - Sunday
+        if (currentDay === 0) currentDay = 7; // костиль
+
+        let prevEnd = null;
+
+        for (let i = 1; i < 9; i++) { // 1-8
+            let endH = getHours(i)[1];
+
+            let end = timeToDate(endH);
+
+            if (date < end && (!prevEnd || date > prevEnd)) {
+                return {
+                    day: currentDay,
+                    num: i
+                }
+            } else {
+                prevEnd = end;
+            }
+        }
+
+        return {
+            day: -1,
+            num: -1
+        }
     }
 
-    componentDidMount() {
-        if(this.props.onReady) this.props.onReady(this.ref.current);
-    }
-
-    makeTable(size, elements) {
+    const makeTable = (size, elements) => {
         const cells = [];
-        let active = this.getActiveLesson();
+        let active = getActiveLesson();
 
         for (let i = 0; i < size.rows + 1; i++) {
             for (let j = 0; j < size.days + 1; j++) {
                 // first row
                 if (i === 0) {
                     if (j === 0) {
-                        cells.push(<EmptyCell key="first"/>); // first cell always empty
+                        cells.push(<EmptyCell key="first" />); // first cell always empty
                     } else {
-                        const weekday = this.getWeekday(j);
-                        cells.push(<DayCell weekday={weekday} key = {weekday}/>);
+                        const weekday = getWeekday(j);
+                        cells.push(<DayCell weekday={weekday} key={weekday} />);
                     }
                 } else {
                     if (j === 0) {
-                        cells.push(<NumerationCell key={i} num={i} time={this.getHours(i)}/>);
+                        cells.push(<NumerationCell key={i} num={i} time={getHours(i)} />);
                     } else {
                         const lesson = elements.find(el => el.position === i && el.day === j)?.lesson;
-                        cells.push(<LessonCell key={i+":"+j} lesson={lesson} active={active.day === j && active.num === i}/>)
+                        cells.push(<LessonCell key={i + ":" + j} lesson={lesson} active={active.day === j && active.num === i} />)
                     }
                 }
             }
@@ -60,136 +137,55 @@ class TimetableComponent extends React.Component {
         return cells;
     }
 
-    getActiveLesson() {
-    	let date = getCurrentUADate();
-    	let currentDay = date.getDay(); // 0 - Sunday
-    	if(currentDay === 0) currentDay = 7; // костиль
+    useEffect(() => {
+        if (elements) onReady?.(ref.current);
+    }, [elements]);
 
-    	let prevEnd = null;
-
-    	for(let i = 1; i < 9; i++) { // 1-8
-    		let endH = this.getHours(i)[1];
-    		
-    		let end = this.timeToDate(endH);
-
-    		if(date < end && (!prevEnd || date > prevEnd)) {
-    			return {
-    				day: currentDay,
-    				num: i
-    			}
-    		} else {
-    			prevEnd = end;
-    		}
-    	}
-
-    	return {
-    		day: -1,
-    		num: -1
-    	}
-    }
-
-    timeToDate(time) {
-    	let date = getCurrentUADate();
-    	let [hours, minutes] = time.split(':');
-    	date.setHours(hours);
-    	date.setMinutes(minutes)
-    	date.setSeconds(0);
-    	return date;
-    }
-
-    // TODO переписати ці світчі на щось нормальне
-    getWeekday(number) {
-        switch (number) {
-            case 1:
-                return "Понеділок";
-            case 2:
-                return "Вівторок";
-            case 3:
-                return "Середа";
-            case 4:
-                return "Четвер";
-            case 5:
-                return "П'ятниця";
-            case 6:
-                return "Субота";
-            case 7:
-                return "Неділя";
-            default:
-                return "Не вдалося отримати день"
-        }
-    }
-
-    getHours(num) {
-        switch(num) {
-            case 1:
-                return ["8:30", "10:05"];
-            case 2:
-                return ["10:20", "11:55"];
-            case 3:
-                return ["12:10", "13:45"];
-            case 4:
-                return ["14:15", "15:50"];
-            case 5:
-                return ["16:00", "17:35"];
-            case 6:
-                return ["17:40", "19:15"];
-            case 7:
-                return ["19:20", "20:55"];
-            case 8:
-                return ["21:00", "22:35"];
-            default:
-                return ["", ""];
-        }
-    }
-
-    findSize(elements) {
-        let days = 5;
-        let rows = 5;
-        for (let element of elements) {
-            if (element.day > days) days = element.day;
-            if (element.position > rows) rows = element.position;
-        }
-        return {
-            days: Math.min(days, 7),
-            rows
-        }
-    }
+    return (
+        <div className={classNames({
+            timetable: true,
+            "has-saturday": sizes.days === 6,
+            "has-sunday": sizes.days === 7, // Не заздрю...
+        })} ref={ref}>
+            {makeTable(sizes, elements)}
+        </div>
+    )
 }
 
-const NumerationCell = ({num, time}) => {
+const NumerationCell = ({ num, time }) => {
     return (
-            <div className={classNames({
-                "timetable-cell": true,
-                "numeration": true,
-            })}>
-                <div className="num">{num}</div>
-                <div className="hours">
-                    <div>{time[0]}</div>
-                    <div>{time[1]}</div>
-                </div>
+        <div className={classNames({
+            "timetable-cell": true,
+            "numeration": true,
+        })}>
+            <div className="num">{num}</div>
+            <div className="hours">
+                <div>{time[0]}</div>
+                <div>{time[1]}</div>
             </div>
-        )
+        </div>
+    )
 }
 
 const EmptyCell = () => {
     return (
         <div className={classNames({
-                "timetable-cell": true,
-                "empty": true
-            })}>
+            "timetable-cell": true,
+            "empty": true
+        })}>
         </div>
-        )
+    )
 }
 
-const DayCell = ({weekday}) => {
+const DayCell = ({ weekday }) => {
     return (
         <div className={classNames({
-                "timetable-cell": true,
-                "date": true
-            })}>
+            "timetable-cell": true,
+            "date": true
+        })}>
             <div className="date">{weekday}</div>
         </div>
-        )
+    )
 }
 
 class LessonCell extends React.Component {
@@ -216,7 +212,7 @@ class LessonCell extends React.Component {
                     "active": active
                 })}>
                     {children}
-                    <LessonFragment lesson={fadeOut ? prevLesson : lesson}/>
+                    <LessonFragment lesson={fadeOut ? prevLesson : lesson} />
                 </div>
             </div>
         )
@@ -246,9 +242,9 @@ const LessonFragment = ({ lesson }) => {
     if (!lesson) return null;
     return (
         <div className={classNames({
-                lesson: true,
-                [lesson.type]: true
-            })}>
+            lesson: true,
+            [lesson.type]: true
+        })}>
             <div className="number">{lesson.number}</div>
             <div className="subject">{lesson.subject}</div>
             <div className="lecturer">{lesson.lecturer}</div>
